b9e2ae0fc5d16eda52064de49e366c0fc1e1191e
mrkamiali <kamiali63@gmail.com>
2018-08-26 14:57:45 +0500

    class work of August 26 2018

---
+++ 26Aug2018/LinearLayoutDemo/app/src/main/java/com/axiom/linearlayoutdemo/DemoHorizontal.kt
@@ -0,0 +1,12 @@
+package com.axiom.linearlayoutdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoHorizontal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_horizontal)
+    }
+}
