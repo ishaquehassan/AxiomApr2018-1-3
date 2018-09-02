59b2683fbb2dfddb04adf7f36bcb575aaf39b3db
mrkamiali <kamiali63@gmail.com>
2018-09-02 16:13:46 +0500

    class work of 2 september 2018

---
+++ Demo1/app/src/main/java/com/axiom/demo1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.axiom.demo1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var myEditText = findViewById<EditText>(R.id.myEditText)
+//        var myButton1 = findViewById<Button>(R.id.myButton)
+//
+//
+//        myButton1.setOnClickListener(object: View.OnClickListener{
+//            override fun onClick(p0: View?) {
+//
+//            }
+//        })
+//        myButton.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(p0: View?) {
+//
+//            }
+//        })
+
+
+    }
+}
